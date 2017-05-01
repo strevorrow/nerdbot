22b0244b18bb34c9e15baeb79aee83669b04a77d
Stephen Trevorrow <strevorrow@confluence.com>
2017-05-01 14:41:12 -0400

    adding new command test

---
+++ src/commands/response.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const _ = require('lodash')
+const config = require('../config')
+
+const msgDefaults = {
+  response_type: 'in_channel',
+  username: 'Nerdbot',
+  icon_emoji: config('ICON_EMOJI')
+}
+
+const handler = (payload, res) => {
+	if(payload.text.indexOf("hi") > -1)
+	{
+		let msg = _.defaults({
+		channel: payload.channel_name,
+		attachments: payload }, msgDefaults)
+	}
+	else{
+	  let msg = _.defaults({
+		channel: payload.channel_name,
+		attachments: payload
+	  }, msgDefaults)
+	}
+
+  res.set('content-type', 'application/json')
+  res.status(200).json(msg)
+  return
+}
+
+module.exports = { pattern: /response/ig, handler: handler }
