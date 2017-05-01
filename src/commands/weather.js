62e0ec514a68159596490e648e679e19ff13ea69
Stephen Trevorrow <strevorrow@confluence.com>
2017-05-01 16:18:47 -0400

    validates

--- src/commands/weather.js
+++ src/commands/weather.js
@@ -3,6 +3,7 @@
 const _ = require('lodash')
 const config = require('../config')
 var request = require('request');
+var Regex = require("regex");
 
 const msgDefaults = {
   response_type: 'in_channel',
@@ -13,25 +14,41 @@ const msgDefaults = {
 const handler = (payload, res) => {
 	var zipCode = payload.text.substring(8);
 	var weatherText = "";
-	var zipCodeRegex = ^\d{5}(?:[-\s]\d{4})?$;
-	var result = re.exe(zipCode);
-	console.log(result);
-	request.get("http://api.openweathermap.org/data/2.5/weather?zip="+zipCode+"&appid=" + config('WEATHER_API_KEY'), function (error, response, body) {
-		var weatherJson = JSON.parse(body)
-		weatherText = "It is currently " + (weatherJson["main"]["temp"] * (9/5) - 459.67) + " degrees Fahrenheit. " + "The dominant weather pattern is: " + weatherJson["weather"][0]["main"];
+	var regex = new Regex(^\d{5}(?:[-\s]\d{4})?$);
+
+	if(regex.test(zipCode){
+		request.get("http://api.openweathermap.org/data/2.5/weather?zip="+zipCode+"&appid=" + config('WEATHER_API_KEY'), function (error, response, body) {
+			var weatherJson = JSON.parse(body)
+			weatherText = "It is currently " + (weatherJson["main"]["temp"] * (9/5) - 459.67) + " degrees Fahrenheit. " + "The dominant weather pattern is: " + weatherJson["weather"][0]["main"];
+			let msg = _.defaults({
+			channel: payload.channel_name,
+			attachments: [{
+							title: 'Weather Report :national_park:',
+							color: '#2FA44F',
+							text: weatherText,
+							mrkdwn_in: ['text']
+							}]}, msgDefaults)
+		
+			res.set('content-type', 'application/json')
+			res.status(200).json(msg)
+			return
+		});
+	}
+	else{
 		let msg = _.defaults({
-		channel: payload.channel_name,
-		attachments: [{
-						title: 'Weather Report :national_park:',
-						color: '#2FA44F',
-						text: weatherText,
-						mrkdwn_in: ['text']
-						}]}, msgDefaults)
-	
-		res.set('content-type', 'application/json')
-		res.status(200).json(msg)
-		return
-	});
+			channel: payload.channel_name,
+			attachments: [{
+							title: 'Invalid Zip Code :negative_squared_cross_mark:',
+							color: '#2FA44F',
+							text: "Please use a correctly formatted zip code",
+							mrkdwn_in: ['text']
+							}]}, msgDefaults)
+		
+			res.set('content-type', 'application/json')
+			res.status(200).json(msg)
+			return
+		
+	}
 }
 
 module.exports = { pattern: /weather/ig, handler: handler }
