a48e9dd06b4e7b05fd3686dfd8ba25e12d813320
Stephen Trevorrow <strevorrow@confluence.com>
2017-05-01 16:11:51 -0400

    weird scoping rules

--- src/commands/weather.js
+++ src/commands/weather.js
@@ -17,20 +17,19 @@ const handler = (payload, res) => {
 	request.get("http://api.openweathermap.org/data/2.5/weather?zip="+zipCode+"&appid=" + config('WEATHER_API_KEY'), function (error, response, body) {
 		var weatherJson = JSON.parse(body)
 		weatherText = "It is currently " + (weatherJson["main"]["temp"] * (9/5) - 459.67) + " degrees Fahrenheit. " + "The dominant weather pattern is: " + weatherJson["weather"][0]["main"];
-	});
-	console.log(weatherText)
-	let msg = _.defaults({
+		let msg = _.defaults({
 		channel: payload.channel_name,
 		attachments: [{
-						title: 'N/A',
+						title: 'Weather Report :national_park:',
 						color: '#2FA44F',
 						text: weatherText,
 						mrkdwn_in: ['text']
 						}]}, msgDefaults)
 	
-	res.set('content-type', 'application/json')
-	res.status(200).json(msg)
-	return
+		res.set('content-type', 'application/json')
+		res.status(200).json(msg)
+		return
+	});
 }
 
 module.exports = { pattern: /weather/ig, handler: handler }
