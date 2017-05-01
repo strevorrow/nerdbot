89d5dc11f024b65f0a5dbfd6ddb44ebfd2a9f57b
Stephen Trevorrow <strevorrow@confluence.com>
2017-05-01 16:08:44 -0400

    generally working

--- src/commands/weather.js
+++ src/commands/weather.js
@@ -12,36 +12,21 @@ const msgDefaults = {
 
 const handler = (payload, res) => {
 	var zipCode = payload.text.substring(8);
-
+	var weatherText = "";
+	
 	request.get("http://api.openweathermap.org/data/2.5/weather?zip="+zipCode+"&appid=" + config('WEATHER_API_KEY'), function (error, response, body) {
 		var weatherJson = JSON.parse(body)
-		console.log(weatherJson)
-		console .log(weatherJson["main"])
-		console .log(weatherJson["main"]["temp"])
-		var weatherText = "It is currently " + (weatherJson["main"]["temp"] * (9/5) - 459.67) + " degrees Fahrenheit"
-		console.log(weatherText)
-		let msg = _.defaults({
-			channel: payload.channel_name,
-			attachments: [{
-							title: 'N/A',
-							color: '#2FA44F',
-							text: weatherText,
-							mrkdwn_in: ['text']
-							}]}, msgDefaults)
-		
-		res.set('content-type', 'application/json')
-		res.status(200).json(msg)
-		return
+		var weatherText = "It is currently " + (weatherJson["main"]["temp"] * (9/5) - 459.67) + " degrees Fahrenheit. " + "The dominant weather pattern is: " + weatherJson["weather"][0]["main"];
 	});
 	
 	let msg = _.defaults({
-	channel: payload.channel_name,
-	attachments: [{
-					title: 'N/A',
-					color: '#2FA44F',
-					text: 'Hello.',
-					mrkdwn_in: ['text']
-					}]}, msgDefaults)
+		channel: payload.channel_name,
+		attachments: [{
+						title: 'N/A',
+						color: '#2FA44F',
+						text: weatherText,
+						mrkdwn_in: ['text']
+						}]}, msgDefaults)
 	
 	res.set('content-type', 'application/json')
 	res.status(200).json(msg)
