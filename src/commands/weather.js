edbbbe6f64e11d25b196ac46bcdc52e92e6bc03c
Stephen Trevorrow <strevorrow@confluence.com>
2017-05-01 15:54:13 -0400

    testing temp

--- src/commands/weather.js
+++ src/commands/weather.js
@@ -12,12 +12,20 @@ const msgDefaults = {
 
 const handler = (payload, res) => {
 	var zipCode = payload.text.substring(8);
-	console.log(zipCode);
 
 	request.get("http://api.openweathermap.org/data/2.5/weather?zip="+zipCode+"&appid=" + config('WEATHER_API_KEY'), function (error, response, body) {
-	  console.log('error:', error); // Print the error if one occurred 
-	  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
-	  console.log('body:', body); // Print the HTML for the Google homepage. 
+		let msg = _.defaults({
+			channel: payload.channel_name,
+			attachments: [{
+							title: 'N/A',
+							color: '#2FA44F',
+							text: 'It is currently ' + body["main"]["temp"] * (9/5) - 459.67 + ' degrees Fahrenheit',
+							mrkdwn_in: ['text']
+							}]}, msgDefaults)
+		
+		res.set('content-type', 'application/json')
+		res.status(200).json(msg)
+		return
 	});
 	
 	let msg = _.defaults({
