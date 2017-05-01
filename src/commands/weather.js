b5d93a436e227d1beee7d5eec856090200dd94f1
Stephen Trevorrow <strevorrow@confluence.com>
2017-05-01 16:29:09 -0400

    test

--- src/commands/weather.js
+++ src/commands/weather.js
@@ -14,9 +14,8 @@ const msgDefaults = {
 const handler = (payload, res) => {
 	var zipCode = payload.text.substring(8);
 	var weatherText = "";
-	var pattern = /^\d{5}(?:[-\s]\d{4})?$/;
 
-	if(pattern.test(zipCode)){
+	if(zipCode.length == 5)){
 		request.get("http://api.openweathermap.org/data/2.5/weather?zip="+zipCode+"&appid=" + config('WEATHER_API_KEY'), function (error, response, body) {
 			var weatherJson = JSON.parse(body)
 			weatherText = "It is currently " + (weatherJson["main"]["temp"] * (9/5) - 459.67) + " degrees Fahrenheit. " + "The dominant weather pattern is: " + weatherJson["weather"][0]["main"];
