ab5a51c3af612383c6dbabd83f3b8558ba97fac4
Stephen Trevorrow <strevorrow@confluence.com>
2017-05-01 16:15:17 -0400

    adds regex validation on zipcode

--- src/commands/weather.js
+++ src/commands/weather.js
@@ -13,7 +13,9 @@ const msgDefaults = {
 const handler = (payload, res) => {
 	var zipCode = payload.text.substring(8);
 	var weatherText = "";
-	
+	var zipCodeRegex = ^\d{5}(?:[-\s]\d{4})?$;
+	var result = re.exe(zipCode);
+	console.log(result);
 	request.get("http://api.openweathermap.org/data/2.5/weather?zip="+zipCode+"&appid=" + config('WEATHER_API_KEY'), function (error, response, body) {
 		var weatherJson = JSON.parse(body)
 		weatherText = "It is currently " + (weatherJson["main"]["temp"] * (9/5) - 459.67) + " degrees Fahrenheit. " + "The dominant weather pattern is: " + weatherJson["weather"][0]["main"];
